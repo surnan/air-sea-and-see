a656a1bff046a2f35eb1f5cf65e4824e4d663afb
surnan <suresh_nandlall@outlook.com>
2024-05-16 20:37:33 -0400

    Initialize Express and Sequelize with CSRF protection v3

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,13 +1,15 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
 
-// backend/routes/api/index.js
+module.exports = router;
+
+// backend/routes/index.js
+// ...
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 // ...
 
 router.post('/test', function (req, res) {
     res.json({ requestBody: req.body });
 });
-
-
-module.exports = router;
-
