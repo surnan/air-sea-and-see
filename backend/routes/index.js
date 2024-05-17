41e432125c0aa545d43301867a4f661ce68e5d97
surnan <suresh_nandlall@outlook.com>
2024-05-16 21:39:14 -0400

    const { requireAuth } = require(../../utils/auth.js); router.get(   /require-auth,   requireAuth,   (req, res) => {     return res.json(req.user);  OA  } )

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const apiRouter = require('./api');
 router.use('/api', apiRouter);
 
+
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
@@ -17,5 +18,9 @@ router.get("/api/csrf/restore", (req, res) => {
     'XSRF-Token': csrfToken
   });
 });
+
+
+
+
 
 module.exports = router;
