368335215a2e1d98105057e3a3ad726b6d37ba6b
surnan <suresh_nandlall@outlook.com>
2024-05-25 04:06:39 -0400

    Updated migration/02-seed-spots.js for Render Bug

--- backend/db/seeders/02-seed-spot.js
+++ backend/db/seeders/02-seed-spot.js
@@ -53,6 +53,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Spot', null, {}); ///bulkDelete(options, )
+    await queryInterface.bulkDelete(null, {}); ///bulkDelete(options, )
   }
 };
