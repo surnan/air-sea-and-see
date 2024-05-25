ba11dcf7c61f803234f86df99928e2c169ccbc0c
surnan <suresh_nandlall@outlook.com>
2024-05-25 04:44:17 -0400

    Render TS: Disable Spot seeder file to see if build can b sucesful

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -1,14 +1,8 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const {Model, Validator} = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Spot extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // define association here
     }
@@ -57,7 +51,7 @@ module.exports = (sequelize, DataTypes) => {
     },
   }, {
     sequelize,
-    modelName: 'Spot',
+    modelName: "Spot",
   });
   return Spot;
 };
