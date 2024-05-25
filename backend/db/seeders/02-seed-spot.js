2d30d65ba90cafa765c074128450814d8f494b0d
surnan <suresh_nandlall@outlook.com>
2024-05-25 05:02:21 -0400

    Render TS: Restore Seeder File for spot

---
+++ backend/db/seeders/02-seed-spot.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const { Spot } = require('../models');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  
+  options.tableName = "Spots"
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await Spot.bulkCreate([ 
+      {
+        ownerId: 3,
+        address: "One Park Avenue",
+        city: "New York City",
+        state: "NY",
+        country: "USA",
+        lat: 40.746052095477836,
+        lng: -73.98142495133493,
+        name: "NYU Langone Headquarters",
+        description: "Sleep, get healed, get paid !!",
+        price: 3000,
+      },
+      {
+        ownerId: 1,
+        address: "78 Cottage Street",
+        city: "Jersey City",
+        state: "NJ",
+        country: "USA",
+        lat: 40.73546808655058,
+        lng: -74.0622927904529,
+        name: "Peachtree Suites",
+        description: "Sleep, recharge, privacy & high quality & cheap price",
+        price: 1000,
+      },
+      {
+        ownerId: 2,
+        address: "1072 Elder Ave",
+        city: "Bronx",
+        state: "NY",
+        country: "USA",
+        lat: 40.82595300239568,
+        lng: -73.87838826939743,
+        name: "Boogy Down Bronx",
+        description: "Where Hip Hop as born",
+        price: 75,
+      }
+    ],
+      { validate: true },
+      options);
+  },
+
+  async down(queryInterface, Sequelize) {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      name: { [Op.in]: ['NYU Langone Headquarters', 'Peachtree Suites', 'Boogy Down Bronx'] }
+    },{});
+  }
+};
